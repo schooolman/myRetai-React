1ff489361a02829e87dfb93b708b9e2e3a5c5430
Jake Schoolmeesters <j.schoolmee@gmail.com>
2017-10-04 21:53:37 -0500

    added in star rating functionality.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -17,7 +17,29 @@ class Comments extends React.Component {
           </div>
         </div>
         <div className="review-pro">
-          <div className={this.props.pro.overallRating} id="star"></div>
+          {(() => {
+                 switch (this.props.pro.overallRating) {
+                   case "0":
+                    return <img className="rating" src="images/zero-star.png" />
+                    ;
+                   case "1":
+                    return <img className="rating" src="images/one-star.png" />
+                     ;
+                   case "2":
+                    return <img className="rating" src="images/two-star.png" />
+                     ;
+                   case "3":
+                    return <img className="rating" src="images/three-star.png" />
+                     ;
+                   case "4":
+                    return <img className="rating" src="images/four-star.png" />
+                     ;
+                   case "5":
+                    return <img className="rating" src="images/five-star.png" />
+                     ;
+                   default: return <h4>Not Available</h4>;
+                 }
+               })()}
           <h1>{this.props.pro.overallRating}</h1>
           <h1>{this.props.pro.title}</h1>
           <p>{this.props.pro.review}</p>
@@ -25,8 +47,29 @@ class Comments extends React.Component {
           <span>{this.props.pro.datePosted}</span>
         </div>
         <div className="review-con">
-          <div className={this.props.con.overallRating} id="star"></div>
-          <h1>{this.props.con.overallRating}</h1>
+          {(() => {
+                 switch (this.props.con.overallRating) {
+                   case "0":
+                    return <img className="rating" src="images/zero-star.png" />
+                    ;
+                   case "1":
+                    return <img className="rating" src="images/one-star.png" />
+                     ;
+                   case "2":
+                    return <img className="rating" src="images/two-star.png" />
+                     ;
+                   case "3":
+                    return <img className="rating" src="images/three-star.png" />
+                     ;
+                   case "4":
+                    return <img className="rating" src="images/four-star.png" />
+                     ;
+                   case "5":
+                    return <img className="rating" src="images/five-star.png" />
+                     ;
+                   default: return <h4>Not Available</h4>;
+                 }
+               })()}
           <h1>{this.props.con.title}</h1>
           <p>{this.props.con.review}</p>
           <span>{this.props.con.screenName}</span>
